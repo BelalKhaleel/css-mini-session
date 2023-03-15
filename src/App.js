47ced2b44eb8e894121942707981ed60220528d5
fadi <fadi@codi.tech>
2023-03-15 14:31:24 +0200

    Everything Pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Toggle from './components/toggle/toggle';
+import image from '../src/images/outside.jpg'
+import { useState } from 'react';
 function App() {
+  const [isDark, setIsDark] = useState(false);
+  const theme = isDark ? 'dark-theme' : 'light-theme';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <Toggle isDark={isDark} setIsDark={setIsDark} />
+      {/* <div className='card'>
+  <img src={image}/> 
+  <div className='cardDetails'>
+    <h2>Title</h2></div> 
+</div> */}
+
+      <nav className='sideNav'>
+        <div className='logo'></div>
+        <div className='linksWrapper'>
+
+          <div className='linkContainer'>
+            <a>Home</a>
+            <span>Home</span>
+          </div>
+          <div className='linkContainer'>
+            <a>Reports</a>
+            <span>Reports</span>
+          </div>
+          <div className='linkContainer'>
+            <a>Dashboard</a>
+            <span>Dashboard</span>
+          </div>
+          <div className='linkContainer'>
+            <a>About Emile</a>
+            <span>AboutEmile</span>
+          </div>
+          <div className='linkContainer'>
+            <a>Settings</a>
+            <span>Settings</span>
+          </div>
+
+
+        </div>
+        <div className='linksBottomWrapper'>
+
+
+          <div className='linkContainer'>
+            <a>Logout</a>
+            <span>Logout</span>
+          </div>
+
+
+        </div>
+      </nav>
     </div>
   );
 }
